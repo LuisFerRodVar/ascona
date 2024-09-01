082dfbb740e1d32b16530ae32203e851c1c8ad89
LuisFerRodVar <luisfrv1@estudiantec.cr>
2024-09-01 13:58:55 -0600

    Added basic styles for navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './core/navbar/navbar.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { WhoWeAreComponent } from './who-we-are/who-we-are.component';
+import { ProgramsComponent } from './programs/programs.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NavbarComponent,FooterComponent,HeaderComponent,WhoWeAreComponent,ProgramsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  translate: TranslateService = inject(TranslateService);
+
+  translateText(lang: string) {
+    this.translate.use(lang)
+  }
 
 }
