4e95a040afbf0ae65ca55e8ba826594dee0af355
LuisFerRodVar <luisfrv1@estudiantec.cr>
2024-09-01 13:15:58 -0600

    Added internacionalization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,31 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { withInterceptorsFromDi } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+export function createTranslateLoader(http: HttpClient){
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    importProvidersFrom([
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient]
+        },
+        defaultLanguage:'es'
+      })
+    ]),
+    provideClientHydration(),
+    provideHttpClient(withInterceptorsFromDi())
+  ]
 };
